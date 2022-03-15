8dfa452f8a0cc86b1e0864005f8d0b2d6ef78441
BuildTools <unconfigured@null.spigotmc.org>
2022-03-15 23:42:23 +0100

    Js and css changes

--- ChallengesPage/js/main.js
+++ ChallengesPage/js/main.js
@@ -1,7 +1,7 @@
-const spans = document.querySelectorAll("span");
+/*const spans = document.querySelectorAll("span");
 for(let i = 0; i < spans.length; i++){
   spans[i].onanimationend = () => {
     console.log("oui");
     spans[i].style.display = "contents";
   };
-}
+}*/
